1330f3dc96d0bb6c338150f8a1ae305c1669a6b0
Jerome Etienne <jerome.etienne@gmail.com>
2025-10-16 22:09:28 +0200

    Refactor animator_matplotlib.py: reorganize imports and enhance canvas and viewport initialization

--- examples/animator_matplotlib.py
+++ examples/animator_matplotlib.py
@@ -2,13 +2,18 @@
 Basic example of creating and rendering a simple GSP scene with matplotlib.
 """
 
+# pip imports
 import numpy as np
 import os
 import gsp
 import gsp_matplotlib
+import mpl3d.glm
+import matplotlib.pyplot
 
+# local imports
 from common.gsp_animator import GspAnimatorMatplotlib
 from common.fps_monitor import FpsMonitor
+from common.mesh_parser import MeshParserMeshio
 
 __dirname__ = os.path.dirname(os.path.abspath(__file__))
 # Set random seed for reproducibility
@@ -18,15 +23,9 @@ np.random.seed(10)
 ###############################################################################
 # Create a GSP scene
 #
-canvas = gsp.core.Canvas(width=256, height=256, dpi=100)
-viewport = gsp.core.Viewport(
-    origin_x=0,
-    origin_y=0,
-    width=canvas.width,
-    height=canvas.height,
-    background_color=gsp.Constants.White,
-)
-canvas.add(viewport=viewport)
+canvas = gsp.core.Canvas(512, 512, 100)
+viewport = gsp.core.Viewport(0, 0, canvas.width, canvas.height, gsp.Constants.White)
+canvas.add(viewport)
 
 ###############################################################################
 # Add some random points
@@ -38,6 +37,15 @@ colors_np = np.array([gsp.Constants.Green for _ in range(n_points)], np.float32)
 pixels = gsp.visuals.Pixels(positions_np, sizes_np, colors_np)
 viewport.add(pixels)
 
+# =============================================================================
+# Add a mesh
+# =============================================================================
+
+obj_mesh_path = f"{__dirname__}/data/bunny.obj"
+vertices_coords, faces_indices, uvs_coords, normals_coords = MeshParserMeshio.parse_obj_file(obj_mesh_path)
+vertices_coords = mpl3d.glm.fit_unit_cube(vertices_coords)
+mesh = gsp.visuals.Mesh(vertices_coords, faces_indices, cmap=matplotlib.pyplot.get_cmap("magma"), edgecolors=(0, 0, 0, 0.25))  # type: ignore
+viewport.add(mesh)
 
 ###############################################################################
 # Render the scene with matplotlib
